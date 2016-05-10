2e7d9dbdcebd3db5aeb026830219a905594e828d
Neisha Fannin <Ms.Latriece@yahoo.com>
2016-05-10 13:48:15 -0400

    FinalProject
    Final Exam

---
+++ FinalProject/Elevator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+
+            for (int i = 0; i < Occupants.Length; i++)
+            {
+                Passenger p = Occupants[i];
+                if (p != null)
+                {
+                    totalWeight += p.GetWeight();
+                }
+            }
+
+            return totalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {      
+
+            if (GetCurrentWeight() > MaxWeight)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+    
+            }
+     
+        }
+
+
+    }
+}
---
+++ FinalProject/Passenger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+
+        }
+
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+
+
+
+    }
+}
